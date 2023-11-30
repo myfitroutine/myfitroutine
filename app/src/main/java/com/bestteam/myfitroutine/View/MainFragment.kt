5e450a1ff812bb2db8d2690a25f31af492ba1fa0
yoonhee~ <flvps123@gmail.com>
2023-11-30 17:11:13 +0900

    [Design] main_page today weight 구현

--- app/src/main/java/com/bestteam/myfitroutine/View/MainFragment.kt
+++ app/src/main/java/com/bestteam/myfitroutine/View/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.bestteam.myfitroutine.View
 
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
 import com.bestteam.myfitroutine.Dialog.TodayWeightDialog
@@ -14,7 +16,12 @@ import com.bestteam.myfitroutine.Model.WeightData
 import com.bestteam.myfitroutine.R
 import com.bestteam.myfitroutine.ViewModel.MainViewModel
 import com.bestteam.myfitroutine.databinding.FragmentMainBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -29,6 +36,7 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -36,32 +44,35 @@ class MainFragment : Fragment() {
         val todayWeight = binding.txtTodayWeight
 
         viewLifecycleOwner.lifecycleScope.launch {
+            weightViewModel.getTodayWeight()
             weightViewModel.todayWeight.collect { newWeight ->
-                Log.d("nyh main", "onViewCreated: $newWeight")
-                todayWeight.text = newWeight ?: "없어요"
+                withContext(Dispatchers.Main) {
+                    Log.d("nyh main", "onViewCreated: $newWeight")
+                    todayWeight.text = (newWeight ?: "없어요").toString()
+                }
             }
         }
-
         binding.btnTodayWeight.setOnClickListener {
             val todayWeightDialog = TodayWeightDialog()
             todayWeightDialog.show(childFragmentManager, "TodayWeight")
         }
-
     }
 
     override fun onResume() {
         super.onResume()
 
-        weightViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        weightViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val todayWeight = binding.txtTodayWeight
 
         viewLifecycleOwner.lifecycleScope.launch {
+            weightViewModel.getTodayWeight()
             weightViewModel.todayWeight.collect { newWeight ->
-                Log.d("nyh main", "onresume: $newWeight")
-                todayWeight.text = newWeight ?: "없어요"
+                withContext(Dispatchers.Main) {
+                    Log.d("nyh main", "onViewCreated: $newWeight")
+                    todayWeight.text = (newWeight ?: "없어요").toString()
+                }
             }
         }
-
     }
 
 }
