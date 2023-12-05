0d46c3c0f9857e9ee5aca4f636cd8b20d670da26
suyeno <realsu_yeonini418@naver.com>
2023-12-05 22:07:39 +0900

    [Feat] 로그인 기능 구현

--- app/src/main/java/com/bestteam/myfitroutine/LogIn/LogInActivity.kt
+++ app/src/main/java/com/bestteam/myfitroutine/LogIn/LogInActivity.kt
@@ -1,20 +1,18 @@
 package com.bestteam.myfitroutine.LogIn
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bestteam.myfitroutine.MainActivity
 import com.bestteam.myfitroutine.SignUp.SignUpActivity
 import com.bestteam.myfitroutine.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlin.math.log
 
 class LogInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
-    private var auth : FirebaseAuth? = null
+    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +20,40 @@ class LogInActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val signUpButton = binding.loginSignupButton
+        auth = FirebaseAuth.getInstance()
 
-        signUpButton.setOnClickListener{
+        val signUpButton = binding.loginSignupButton
+        signUpButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
         val logInButton = binding.loginButton
-        logInButton.setOnClickListener{
-            Toast.makeText(this,"로그인에 성공하였습니다.",Toast.LENGTH_LONG).show()현
-            val mainintent = Intent(this,MainActivity::class.java)
-            startActivity(mainintent)
+        logInButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPw.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signIn(email, password)
+            } else if (email.isEmpty()){
+                Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (password.isEmpty()){
+                Toast.makeText(this,"비밀번호를 입력해주세요.",Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth?.signInWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                    val mainIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mainIntent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "로그인에 실패하였습니다.\n 이메일과 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
